68b824cee2c89b97afc9e469a6e899437d03d251
Elias Tracy <elitracy19@gmail.com>
2026-03-22 21:45:21 -0500

    wip: making nodes

--- engine/filesystem/filesystem.go
+++ engine/filesystem/filesystem.go
@@ -2,35 +2,88 @@ package filesystem
 
 import (
 	"fmt"
+	"log"
 	"slices"
 	"strings"
 )
 
-type Node interface {
-	Name() string
-	Parent() *DirNode
-	SetParent(*DirNode)
+type Shell struct {
+	cwd *Node
 }
 
-type DirNode struct {
-	name     string
-	children map[string]Node
-	parent   *DirNode
+func (s *Shell) Ls(path string) string {
+	return s.cwd.Ls(path)
 }
 
-func NewDirNode(name string) *DirNode {
-	return &DirNode{
-		name:     name,
-		children: make(map[string]Node),
+func (s *Shell) Cd(path string) {
+
+	if path == "." {
+		return
 	}
+
+	if path == ".." && s.cwd.Parent() != nil {
+		s.cwd = s.cwd.Parent()
+	}
+
+	dir, remaining, _ := strings.Cut(path, "/")
+
+	node, exists := s.cwd.children[dir]
+
+	// bad cd
+	if !exists {
+		return
+	}
+
+	// at last point
+	if remaining == "" {
+		s.cwd = node
+	}
+
+	if exists && node.IsDir() {
+		if remaining == "" {
+			s.Cd(path)
+			return
+		}
+
+		s.Cd(dir)
+	}
+
 }
 
-func (n *DirNode) Name() string                    { return n.name + "/" }
-func (n *DirNode) AddChild(name string, node Node) { n.children[name] = node; node.SetParent(n) }
-func (n *DirNode) Parent() *DirNode                { return n.parent }
-func (n *DirNode) SetParent(node *DirNode)         { n.parent = node }
+type Node struct {
+	name     string
+	children map[string]*Node
+	parent   *Node
 
-func (n *DirNode) Ls(path string) string {
+	reader func() string
+}
+
+func NewNode(path string) *Node {
+
+	isDir := path[len(path)-1] == '/'
+
+	parts := strings.Split(path, "/")
+
+	node := &Node{
+		name: parts[len(parts)-1],
+	}
+
+	if isDir {
+		log.Printf("NODE: %v", node.name)
+		node.children = make(map[string]*Node)
+
+	}
+
+	return node
+}
+
+func (n *Node) Name() string         { return n.name + "/" }
+func (n *Node) AddChild(node *Node)  { n.children[node.Name()] = node; node.SetParent(n) }
+func (n *Node) Parent() *Node        { return n.parent }
+func (n *Node) SetParent(node *Node) { n.parent = node }
+func (n *Node) IsDir() bool          { return n.children == nil }
+
+func (n *Node) Ls(path string) string {
 
 	if path == ".." {
 		if n.Parent() == nil {
